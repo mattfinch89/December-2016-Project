84e36455a29848f4d8aaaf6bddbc0b3b7221567d
Reyhan Choudhury <reyhanchoudhury@crescentschool.org>
2016-12-14 15:10:20 -0500

    Added a way of determining which column the tile was placed in. And if there is already a tile placed there.

--- src/Connect.java
+++ src/Connect.java
@@ -4,13 +4,33 @@ public class Connect implements Grid {
 	int rounds = 0;
 	int width;
 	int height;
+	int [][] column = new int[6][5];
+
 	
-	public void onPlacedX(int x, int y){ //coordinates of move
+	public Connect()
+	{
+		for (int i = 0; i <= 5; i++)
+		{
+			for(int j = 0; j <= i; j++)
+			column[i][j] = 0;
+		}
+	}
+	
+	public void onPlacedX(int column){ //coordinates of move
 		
+		for (int i = 0; i <= 5; i++)
+		{
+			if (this.column[column][i] == 0)
+				this.column[column][i] = 1;
+		}
 		rounds++;
 	}
-	public void onPlacedO(int x, int y){ //coordinates of move
-		
+	public void onPlacedO(int column){ //coordinates of move
+		for (int i = 0; i <= 5; i++)
+		{
+			if (this.column[column][i] == 0)
+				this.column[column][i] = 2;
+		}
 		rounds++;
 	}
 	
