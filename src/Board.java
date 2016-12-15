0609ff6c86c2dac0bc2decad40d5d931372741d4
Matt Finch <mattfinch12@shanes-air.crescentschool.local>
2016-12-15 14:06:34 -0500

    Update

--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,23 @@
 
-public class Board implements Grid {
-
-	
+public abstract class Board implements Grid {
 	
 	int width, height;
+	int[][] space; 
+	int winner;
 	
 	@Override
 	public void setGrid(int w, int h) {
 		// TODO Auto-generated method stub
-		this.width = w;
-		this.height = h;
+	
+		this.width = 3;
+		this.height = 3;
+		
+		this.space = new int[w][h];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				this.space[i][j] = 0;
+			}
+ 		}
 	}
 
 	@Override
@@ -35,5 +43,15 @@ public class Board implements Grid {
 		// TODO Auto-generated method stub
 		return this.height;
 	}
+	
+	public int getWinner() {
+		return this.winner;		
+	}
+	
+	public void setWinner(int w) {
+		this.winner = w;
+	}
+
+	
 
 }
